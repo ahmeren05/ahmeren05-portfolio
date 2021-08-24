d150bbceb906310b68767f8745f57ce8b9ff2a61
ahmeren05 <ahmethizireren@gmail.com>
2021-08-25 00:47:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -120,5 +120,4 @@ window.addEventListener("load", () => {
 toupbutton.addEventListener("click",toupfunc)
 function toupfunc() {
   window?.scrollTo({ top: 0?.current?.offsetTop - 48, behavior: "smooth" });
-  document.documentElement.scrollTop= 0
 };
